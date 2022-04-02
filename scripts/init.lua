e4f2165f122735fa2ac44086bf9c385114c9c203
Mike A. Trethewey <miketrethewey@gmail.com>
2022-04-02 00:24:20 -0700

    Cleanup
    Delete zip
    Move overlay numbers
    Move weapons
    Standardize image dimensions
    Verbose item defns
    Lowercase codes
    Legacy locations & maps files
    Satisfy basic variant support

--- scripts/init.lua
+++ scripts/init.lua
@@ -3,9 +3,21 @@ ScriptHost:LoadScript("scripts/custom_item.lua")
 ScriptHost:LoadScript("scripts/ganongoal.lua")
 ScriptHost:LoadScript("scripts/settings.lua")
 local goalsign = GanonGoalItem("Goal Sign", "goalsign")
+
+-- Items
 Tracker:AddItems("items/items.json")
+
+-- Tracker Layout
 Tracker:AddLayouts("layouts/tracker.json")
+
+-- Broadcast Layout
 Tracker:AddLayouts("layouts/broadcast.json")
+
+if Tracker.ActiveVariantUID == "items_only" then
+  -- Default/Items-Only
+  -- Tracker:AddLayouts("variants/" .. Tracker.ActiveVariantUID .. "/layouts/tracker.json")    -- Main Tracker
+  -- Tracker:AddLayouts("variants/" .. Tracker.ActiveVariantUID .. "/layouts/broadcast.json")  -- Broadcast View
+end
 
 if _VERSION == "Lua 5.3" then
     ScriptHost:LoadScript("scripts/autotracking.lua")
