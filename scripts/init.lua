7c2d0cf1f6f1e8733767e923ce31d37c51f01387
Mike A. Trethewey <miketrethewey@gmail.com>
2022-04-01 22:22:30 -0700

    Linting

--- scripts/init.lua
+++ scripts/init.lua
@@ -2,13 +2,13 @@ ScriptHost:LoadScript("scripts/class.lua")
 ScriptHost:LoadScript("scripts/custom_item.lua")
 ScriptHost:LoadScript("scripts/ganongoal.lua")
 ScriptHost:LoadScript("scripts/settings.lua")
-local goalsign = GanonGoalItem("Goal Sign","goalsign")
+local goalsign = GanonGoalItem("Goal Sign", "goalsign")
 Tracker:AddItems("items/items.json")
 Tracker:AddLayouts("layouts/tracker.json")
 Tracker:AddLayouts("layouts/broadcast.json")
 
 if _VERSION == "Lua 5.3" then
     ScriptHost:LoadScript("scripts/autotracking.lua")
-else    
+else
     print("Auto-tracker is unsupported by your tracker version")
 end
