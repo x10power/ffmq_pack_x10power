488f86956c63b229db844a950231e5050af5cf6a
Mike A. Trethewey <miketrethewey@gmail.com>
2022-04-04 23:56:36 -0700

    Add more items
    Add Hats, Jewelry, Mails/Armors, Shields
    Rename toggles
    Convert Magics to overlays
    Remove references to deprecated item files

--- scripts/init.lua
+++ scripts/init.lua
@@ -1,26 +1,15 @@
-ScriptHost:LoadScript("scripts/class.lua")
-ScriptHost:LoadScript("scripts/custom_item.lua")
-ScriptHost:LoadScript("scripts/ganongoal.lua")
-ScriptHost:LoadScript("scripts/settings.lua")
-local goalsign = GanonGoalItem("Goal Sign", "goalsign")
-
 -- Items
-Tracker:AddItems("items/items.json")
-
--- Tracker Layout
-Tracker:AddLayouts("layouts/tracker.json")
-
--- Broadcast Layout
-Tracker:AddLayouts("layouts/broadcast.json")
+--  Armor
+Tracker:AddItems("items/armor.json")
+--  Magics
+Tracker:AddItems("items/magics.json")
+--  Weapons
+Tracker:AddItems("items/weapons.json")
+--  Toggles
+Tracker:AddItems("items/toggles.json")
 
 if Tracker.ActiveVariantUID == "items_only" then
   -- Default/Items-Only
   Tracker:AddLayouts("variants/" .. Tracker.ActiveVariantUID .. "/layouts/tracker.json")    -- Main Tracker
   Tracker:AddLayouts("variants/" .. Tracker.ActiveVariantUID .. "/layouts/broadcast.json")  -- Broadcast View
-end
-
-if _VERSION == "Lua 5.3" then
-    ScriptHost:LoadScript("scripts/autotracking.lua")
-else
-    print("Auto-tracker is unsupported by your tracker version")
 end
