eea2e203a8f61063b4598fc851a20d5c707cfc81
Mike A. Trethewey <miketrethewey@gmail.com>
2022-04-06 21:54:52 -0700

    Verbose filepath

--- resources/tests/items.py
+++ resources/tests/items.py
@@ -6,7 +6,7 @@ codes = []
 dirname = os.path.join(".", "items")
 for filename in os.listdir(dirname):
     if os.path.isfile(os.path.join(dirname, filename)):
-        print(f"Reading: {filename}")
+        print(f"Reading: {dirname}{os.sep}{filename}")
         with open(os.path.join(dirname, filename), "r") as itemsFile:
             itemsManifest = commentjson.load(itemsFile)
             for item in itemsManifest:
