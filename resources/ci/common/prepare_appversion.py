9acc53a6a9cbfce7d2d1264c4381069d541b0316
Mike A. Trethewey <miketrethewey@gmail.com>
2022-04-07 18:13:28 -0700

    Better make build dir

--- resources/ci/common/prepare_appversion.py
+++ resources/ci/common/prepare_appversion.py
@@ -13,8 +13,9 @@ def prepare_appversion():
   with(open(manifest_path)) as ci_settings_file:
     CI_SETTINGS = json.load(ci_settings_file)
 
-  if not os.path.isdir(os.path.join("..", "build")):
-      os.mkdir(os.path.join("..", "build"))
+  buildDir = os.path.join("..","build","scripts")
+  if not os.path.isdir(buildDir):
+      os.makedirs(buildDir)
 
   # set tag to app_version.txt
   if not env["GITHUB_TAG"] == "":
@@ -23,7 +24,7 @@ def prepare_appversion():
           f.seek(0)
           f.write(env["GITHUB_TAG"])
           f.truncate()
-      with open(os.path.join("..","build","scripts","ver.lua"), mode="w+", encoding="utf-8") as ver:
+      with open(os.path.join(buildDir,"ver.lua"), mode="w+", encoding="utf-8") as ver:
           _ = ver.read()
           ver.seek(0)
           ver.write("print(\"Package Version: " + env["GITHUB_TAG"] + "\")")
