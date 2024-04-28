64e0e7acb40e59ddb5d92818dfa147ce2cc318b7
Minnie A. Trethewey (Mike) <minnietrethewey@gmail.com>
2024-04-28 01:24:44 -0700

    Update LocRefs script

--- resources/ci/common/calc_locrefs.py
+++ resources/ci/common/calc_locrefs.py
@@ -4,42 +4,59 @@ from pathlib import Path
 uniques = {"refs": []}
 
 def dig_for_children(parent):
-    ret = parent
+    ret = [parent, [None]]
     if isinstance(parent, dict) and \
         "children" in parent:
-        ret = parent
-        return dig_for_children(parent["children"])
-    if isinstance(parent, list) and \
-        isinstance(parent[0], dict) and \
-        "children" in parent[0]:
-        ret = parent[0]
-        return dig_for_children(parent[0]["children"])
+        ret[0] = parent
+        ret[1].append(dig_for_children(parent["children"]))
+    if isinstance(parent, list):
+        for descendant in parent:
+            if "children" in descendant:
+                ret[0] = descendant
+                ret[1].append(dig_for_children(descendant["children"]))
     return ret
+
+def process_child(parent, child):
+    if isinstance(child, list):
+        for baby in child:
+            process_child(child, baby)
+    if child is not None and "sections" in child:
+        for section in child["sections"]:
+            ref = f"@{child['name']}/{section['name']}"
+            if ref in uniques["refs"]:
+                print(f"ERROR: Dupe Ref! '{ref}'")
+            uniques["refs"].append(ref)
+            print(f"  {ref}")
 
 def calc_locrefs():
     # cycle through locations
     for path in sorted(Path("./locations/").glob("**/*.json")):
-        print(path)
+        print(f"{path}")
         # open locations file
         with path.open("r") as loc_file:
             # load JSON
             loc_json = json.load(loc_file)
             # get first element
-            locations = loc_json[0]
-            print(f" {locations['name']}")
-            # find children
-            children = (dig_for_children(locations))
-            for child in children:
-                if "sections" in child:
-                    for section in child["sections"]:
-                        ref = f"{child['name']}/{section['name']}"
-                        if ref in uniques["refs"]:
-                            print(f"ERROR: Dupe Ref! '{ref}'")
-                        uniques["refs"].append(ref)
-                        print(f"  {ref}")
-                elif isinstance(child, dict):
-                    print(f"NO SECTIONS: {child['name']}")
-        print("")
+            for locations in loc_json:
+                # find children
+                [parent, descendants] = dig_for_children(locations)
+                if "name" in parent:
+                    print(f" {parent['name']}")
+                if descendants is not None:
+                    for descendant in descendants:
+                        if descendant is not None:
+                            for child in descendant:
+                                if child is not None:
+                                    if "sections" in child:
+                                        process_child(parent, child)
+                                    # elif isinstance(child, dict):
+                                    #     print(f"NO SECTIONS: {child['name']}")
+                                    elif isinstance(child, list):
+                                        process_child(parent, child)
+                                    else:
+                                        pass
+                                        # print(child)
+            print("")
 
 if __name__ == "__main__":
     calc_locrefs()
