d5f3e46065693664c8802d5a6fecc55c1aeae72b
Minnie A. Trethewey (Mike) <minnietrethewey@gmail.com>
2024-04-28 22:49:03 -0700

    Do Mac's Ship & Doom Castle

--- scripts/logic/logic.lua
+++ scripts/logic/logic.lua
@@ -56,14 +56,23 @@ end
 
 function aquaria_expert_access()
     return (
-        (has("rivercoin") or fireburg_expert_access()) and has("geminicrest") --gemini warp
+        (has("rivercoin") or fireburg_expert_access()) and
+        has("geminicrest") --gemini warp
     )
 end
 
 function fireburg_expert_access()
     return (
-        (has("suncoin") and has("mobiuscrest") and has("multikey") and has("claw")) or --windia teleport to locked house
-        (has("sandcoin") and has("geminicrest"))--aquaria warp
+        (
+            has("suncoin") and
+            has("mobiuscrest") and
+            has("multikey") and
+            has("claw")
+        ) or --windia teleport to locked house
+        (
+            has("sandcoin") and
+            has("geminicrest")
+        )--aquaria warp
     )
 end
 
@@ -75,14 +84,29 @@ end
 
 function windia_expert_access()
     return (
-        fireburg_access() and has("mobiuscrest") and has("multikey") and has("claw") --fireburg locked house teleport
+        fireburg_access() and
+        has("mobiuscrest") and
+        has("multikey") and
+        has("claw") --fireburg locked house teleport
     )
 end
 
 function doom_castle_access()
-    return (has("suncoin") and has("mobiuscrest") and has("thunderrock") and has("captainscap") and has("megagrenade"))
+    return (
+        has("suncoin") and
+        has("mobiuscrest") and
+        has("thunderrock") and
+        has("captainscap") and
+        has("megagrenade")
+    )
 end
 
 function doom_castle_expert_access()
-    return (windia_expert_access() and has("mobiuscrest") and has("thunderrock") and has("captainscap") and has("megagrenade"))
+    return (
+        windia_expert_access() and
+        has("mobiuscrest") and
+        has("thunderrock") and
+        has("captainscap") and
+        has("megagrenade")
+    )
 end
