6ce8fa32f4be2fd5840a165bb801b4bfa199eac4
x10power <quickfingers90@gmail.com>
2024-05-04 19:29:57 -0400

    Fix my mistake!
    Corrected the chests that were left / right to top and bottom instead!

--- scripts/settings/settings.lua
+++ scripts/settings/settings.lua
@@ -1,6 +1,6 @@
 ------------------------------------------------------------------
 -- Configuration options for scripted systems in this pack
 ------------------------------------------------------------------
-AUTOTRACKER_ENABLE_ITEM_TRACKING = true
+AUTOTRACKER_ENABLE_ITEM_TRACKING = false
 AUTOTRACKER_ENABLE_LOCATION_TRACKING = false -- dont change this unless you know what youre doing
 SHARD_COUNT = "skycoinshard40"
