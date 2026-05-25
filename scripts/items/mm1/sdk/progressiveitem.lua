0c7ab585154eb1553213288cbf5fb9a4f5e4bda4
Minnie A. Trethewey <minnietrethewey@gmail.com>
2026-05-25 16:52:31 -0700

    Update Progressive Items

--- scripts/items/mm1/sdk/progressiveitem.lua
+++ scripts/items/mm1/sdk/progressiveitem.lua
@@ -3,11 +3,13 @@ ProgressiveItem = ProgressiveToggleItem:extend()
 -- LeftClick Override
 -- Next Stage (Not a toggle, go forward, move ahead)
 function ProgressiveItem:onLeftClick()
+    self:setActive(true)
     self:nextStage()
 end
 
 -- RightClick Override
 -- Previous Stage (We're sending you back (to the future))
 function ProgressiveItem:onRightClick()
+    self:setActive(true)
     self:prevStage()
 end
